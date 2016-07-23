0a6a901d578e7d317512fe1af54b6d3e72a38df4
Nathan Wentworth <nwentworth95@gmail.com>
2016-07-23 10:49:18 -0400

    added instructions

--- wwwroot/scripts.js
+++ wwwroot/scripts.js
@@ -1,13 +1,18 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
-var colorcycle = false;
+var colorcycle = true;
+var draw = true;
 
 function Resize() {
 	context.canvas.width = window.innerWidth;
 	context.canvas.height = window.innerHeight;
 }
 Resize();
+
+window.setTimeout (function Text() {
+	document.getElementById('heading').style.display = 'none';
+}, 2000);
 
 function GetMousePos(canvas, evt) {
 	var rect = canvas.getBoundingClientRect();
@@ -21,45 +26,24 @@ canvas.addEventListener('mousemove', function(evt) {
 	var mousePos = GetMousePos(canvas, evt);
 	var widthPercent = (mousePos.x / window.innerWidth);
 	var heightPercent = (mousePos.y / window.innerHeight);
-	if (!colorcycle) {
+	if (colorcycle) {
 		canvas.style.backgroundColor = "hsl(" + (360 * widthPercent) + ',50%,' + (100 * heightPercent) + '%)';		
 	}
-	context.beginPath();
-	context.arc(mousePos.x, mousePos.y, 64, 0, 2 * Math.PI, false);
-	context.fillStyle = 'hsl(' + (360 * widthPercent - 180) + ',50%,' + ((-100 * heightPercent) + 100) + '%)';
-	context.fill();
-	context.stroke();
+	if (draw) {
+		context.beginPath();
+		context.arc(mousePos.x, mousePos.y, 64, 0, 2 * Math.PI, false);
+		context.fillStyle = 'hsl(' + (360 * widthPercent - 180) + ',50%,' + ((-100 * heightPercent) + 100) + '%)';
+		context.fill();
+		context.stroke();
+	}
 }, false);
 
 canvas.addEventListener('click', function () {
-	if (colorcycle) {
-		colorcycle = false;
-	} else {
-		colorcycle = true;
-	}
+	if (colorcycle) { colorcycle = false; }
+	else { colorcycle = true; }
 });
 
-function Draw() {
-	for (var j = 0; j < 50; j++) {
-		var randHeight = Math.floor(Math.random() * (window.innerHeight - 0 + 1) + 0);
-		var randWidth = Math.floor(Math.random() * (window.innerWidth - 0 + 1) + 0);
-		var randColors = [3];
-
-		context.beginPath();
-		context.moveTo((window.innerWidth/2), randHeight);
-		context.lineTo(randWidth, (window.innerHeight/2));
-		context.lineWidth = 3;
-		for (var i = 0; i < 3; i++) {
-			randColors[i] = Math.floor(Math.random() * (255 - 0 + 1) + 0);
-		}
-		context.strokeStyle = 'rgb(0,' + Math.floor(255-5*j) + ',' + Math.floor(255-5*j) + ')';
-		context.stroke();
-	}
-}
-
-// window.setInterval(function(){
-// 	Draw();
-// }, 500);
-
-
-// Draw();
+canvas.addEventListener('keydown', function () {
+	if (draw) { draw = false }
+	else { draw = true }
+})
